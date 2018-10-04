aa042852484bad22ce18d63099a3e789f8a37983
Lee Gaines <eightlimbed@gmail.com>
2018-10-04 12:08:44 -0700

    adds users and state

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -1,12 +1,42 @@
-import React from 'react';
+import axios from 'axios';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import UsersList from './components/UsersList';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            users: []
+        };
+    };
+    componentDidMount() {
+        this.getUsers();
+    };
+    getUsers() {
+        axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
+        .then((res) => { this.setState({ users: res.data.data.users }); })
+        .catch((err) => { console.log(err); });
+    };
+    render() {
+        return (
+            <section className="section">
+                <div className="container">
+                    <div className="columns">
+                        <div className="column is-one-third">
+                            <br />
+                            <h1 className="title is-1">All Users</h1>
+                            <hr/><br/>
+                            <UsersList users={this.state.users}/>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        )
+    }
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
