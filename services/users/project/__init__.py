706151d67f05b358b139d4052bb8783f3a1bc216
Lee Gaines <eightlimbed@gmail.com>
2018-10-04 11:14:12 -0700

    adds flask debug toolbar

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,18 +1,31 @@
 # Configures a route to /users/ping for testing purposes
 
 from flask import Flask
+from flask_debugtoolbar import DebugToolbarExtension
 from flask_sqlalchemy import SQLAlchemy
 import os
 
 # Instantiate the db
 db = SQLAlchemy()
 
+# Instantiate the Debug Toolbar
+toolbar = DebugToolbarExtension()
+
 
 def create_app(script_info=None):
+
+    # Instantiate the app
     app = Flask(__name__)
+
+    # Set configurations
     app_settings = os.getenv('APP_SETTINGS')
     app.config.from_object(app_settings)
+
+    # Instantiate the database
     db.init_app(app)
+
+    # Instantiate the Debug Toolbar
+    toolbar.init_app(app)
 
     # Register blueprints
     from project.api.users import users_blueprint
