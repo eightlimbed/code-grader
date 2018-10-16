8b523b0968ceaa93227e84228950684e73d86fa7
Lee Gaines <eightlimbed@gmail.com>
2018-10-16 12:52:25 -0700

    removes repeat form

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -8,34 +8,56 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-            users: []
+            users: [],
+            username: '',
+            email: '',
         };
         this.addUser = this.addUser.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     };
     componentDidMount() {
         this.getUsers();
     };
+
     addUser(event) {
         event.preventDefault();
-        console.log('sanity check');
+        const data = {
+          username: this.state.username,
+          email: this.state.email
+        };
+        axios.post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
+        .then((res) => { 
+          this.getUsers();
+          this.setState({ username: '', email: '' });
+        })
+        .catch((err) => { console.log(err); });
     };
+
     getUsers() {
         axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
         .then((res) => { this.setState({ users: res.data.data.users }); })
         .catch((err) => { console.log(err); });
     };
+    handleChange(event) {
+        const obj = {};
+        obj[event.target.name] = event.target.value;
+        this.setState(obj);
+    }
     render() {
         return (
             <section className="section">
                 <div className="container">
                     <div className="columns">
                         <div className="column is-half">
-                            <AddUser addUser={this.addUser}/>
+                            <AddUser 
+                              username={this.state.username}
+                              email={this.state.email}
+                              addUser={this.addUser}
+                              handleChange={this.handleChange}
+                            />
                             <br />
                             <h1 className="title is-1">All Users</h1>
                             <hr/><br/>
-                            <AddUser/>
-                            <br/><br/>
                             <UsersList users={this.state.users}/>
                         </div>
                     </div>
